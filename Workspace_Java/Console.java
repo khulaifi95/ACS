21c49bf2d774d5190c3352345ed78a4d03325a69
Zhangda Xu <just4xzd1995@gmail.com>
2019-10-07 14:20:36 +0100

    initiated on my WSL Ubuntu

---
+++ Workspace_Java/Console.java
@@ -0,0 +1,9 @@
+public class Console
+{
+    public static void main(String[] args)
+    {       
+        // Using Console to input data from user
+        String name = System.console().readLine();
+        System.out.println(name);
+    }
+}
---
+++ Workspace_Java/Ex_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex_1 {
+    public static double areaCircle(double radius) {
+        return Math.PI*radius*radius;
+    }
+
+    
+    public void main(String[] args) {
+        Scanner 
+    }    
+}
