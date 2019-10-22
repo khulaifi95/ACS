c91f72f1ea13ae72a1361ecea75eab4dd8a4151d
KevinXu95 <39737553+khulaifi95@users.noreply.github.com>
2019-10-22 16:28:50 +0100

    Lab folders added.

--- Java_Workshop/Workspace/Console.java
+++
@@ -1,9 +0,0 @@
-public class Console
-{
-    public static void main(String[] args)
-    {       
-        // Using Console to input data from user
-        String name = System.console().readLine();
-        System.out.println(name);
-    }
-}
