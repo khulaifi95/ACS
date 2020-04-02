dde7caf340b26cf08ab9c85acafea3308ad69a34
kevinxu95 <just4xzd1995@gmail.com>
2020-04-02 04:02:01 +0100

    Wk12 2 days before CA2 ddl.

--- NISO/CA2/q1.py
+++
@@ -1,8 +0,0 @@
-parsed = ['add', ['mul', ['sub', 5, 1], 3], ['exp', 4, None]]
-operator = ['add', 'sub', 'mul', 'div', 'exp']
-
-stack = []
-def evaluate(parsed):
-    for item in parsed:
-        if item is in operator:
-            stack.append(item)
